c40fca97c17aef3d26a64bcee437c9cd57eebdbc
jeanMACOS <jeantoe@gmail.com>
2012-02-27 20:29:57 +0800

    Remove default Rails page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe "Static Pages" do
   
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "should have the content 'Home'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_content('title',:text => " | Home")
+      page.should have_selector('title',:text => " | Home")
     end
   end
   
@@ -19,8 +19,22 @@ describe "Static Pages" do
   
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
+    end
+  end
+  
+  describe "Contact page" do
+    
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact')
+    end
+    
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    text: "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
     
